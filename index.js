bfa70fa39b5d0ba88c30fcd5f9ebcad0629895fb
Kayquedias <kayquedias204@gmail.com>
2022-03-11 20:27:04 -0300

    player img

--- index.js
+++ index.js
@@ -11,34 +11,26 @@ const speedY = 3.4;
 
 let click = true;
 
+// imagem do jogador principal
+let playerImg = new Image();
+playerImg.src = 'imagens/ship_D.png';
+
 function draw() {
-    drawPlayer()
     canvas.style.backgroundColor = '#000'
+
+    playerImg.addEventListener('load', () => ctx.drawImage(playerImg, windowX / 2 - 25, windowY / 2, 50, 50))
 }
 
-const drawPlayer = (cx = windowX / 2, cy = windowY / 2) => {
-    const playerImg = new Image();
-    playerImg.src = 'imagens/ship_D.png';
-
-    playerImg.addEventListener('load', () => ctx.drawImage(playerImg, cx, cy, 50, 50))
-}
-
-const shoot = (event) => {
-    x = event.offsetX
-    y = event.offsetY
-    d = Math.sqrt(Math.pow(Math.abs(windowX / 2 - x),2) + Math.pow(Math.abs(windowY / 2 - y),2));
-    
+const shoot = () => {
     bullet = {
         x : windowX / 2,
         y : windowY / 2,
-        xChange : (x - windowX / 2)/(d / speedX),
-        yChange : (y - windowY / 2)/(d / speedY),
     }; 
     bullets.push(bullet);
 }
 
 function drawBullet(x, y) {
-    ctx.fillStyle = '#000';
+    ctx.fillStyle = '#fff';
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, Math.PI * 2);
     ctx.closePath();
@@ -48,8 +40,8 @@ function drawBullet(x, y) {
 const bullets = [];
 
 function update(i) {
-    bullets[i].x += bullets[i].xChange;
-    bullets[i].y += bullets[i].yChange;
+    bullets[i].y -= speedY;
+    // bullets[i].y += bullets[i].yChange;
 }
 
 function animate() {
@@ -59,16 +51,19 @@ function animate() {
         drawBullet(element.x, element.y)
         update(i)
     });
-    draw()
+
+    ctx.drawImage(playerImg, windowX / 2 - 25, windowY / 2, 50, 50)
 
     requestAnimationFrame(animate);
 }
 
-canvas.addEventListener('click', event => {
-    shoot(event)
+canvas.addEventListener('click', () => {
+    shoot()
 
     if (click) {
         animate();
         click = false
     }
 })
+
+draw()
