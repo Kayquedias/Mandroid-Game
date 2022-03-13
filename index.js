ae3e1f21e84cc2879ca21493166ac98f98dec859
Kayquedias <kayquedias204@gmail.com>
2022-03-13 14:17:41 -0300

    player movements

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector('#canvas1');
 const ctx = canvas.getContext('2d');
+
+const canvas2 = document.querySelector('#canvas2');
+const ctx2 = canvas2.getContext('2d');
 
 const windowX = window.innerWidth;
 const windowY = window.innerHeight;
 canvas.width = windowX;
 canvas.height = windowY;
+canvas2.width = 50;
+canvas2.height = 50;
 
-const speedX = 3.4;
-const speedY = 3.4;
+const speedX = 2;
+const speedY = 2;
 
-let click = true;
+let isPressed = true;
+
+let playerLocation = {
+    x: windowX / 2,
+    y: windowY / 2
+};
 
 // imagem do jogador principal
 let playerImg = new Image();
@@ -18,52 +28,93 @@ playerImg.src = 'imagens/ship_D.png';
 function draw() {
     canvas.style.backgroundColor = '#000'
 
-    playerImg.addEventListener('load', () => ctx.drawImage(playerImg, windowX / 2 - 25, windowY / 2, 50, 50))
+    playerImg.addEventListener('load', () => ctx.drawImage(playerImg, playerLocation.x, playerLocation.y, 50, 50))
 }
+
+function rotatePlayer() {
+    ctx2.clearRect(0, 0, 50, 50)
+    ctx2.translate(25, 25);
+    ctx2.rotate(2 * Math.PI / 180);
+    ctx2.translate(-25, -25);
+    ctx2.drawImage(playerImg, playerLocation.x, playerLocation.y, 50, 50)
+};
 
 const shoot = () => {
     bullet = {
-        x : windowX / 2,
-        y : windowY / 2,
+        x : playerLocation.x,
+        y : playerLocation.y
     }; 
     bullets.push(bullet);
-}
+};
 
 function drawBullet(x, y) {
     ctx.fillStyle = '#fff';
     ctx.beginPath();
-    ctx.arc(x, y, 3, 0, Math.PI * 2);
+    ctx.arc(x + 25, y, 3, 0, Math.PI * 2);
     ctx.closePath();
     ctx.fill();
-}
+};
 
 const bullets = [];
 
-function update(i) {
-    bullets[i].y -= speedY;
-    // bullets[i].y += bullets[i].yChange;
-}
+function update(i, ArrLeft = false, ArrRight = false, ArrUp = false, ArrDown = false) {
+    // Velocidade do tiro 
+    if (i >= 0) {
+        // bullets[i].x += speedX;
+        bullets[i].y -= speedY;
+    }
+    
+    // Movimentação do player
+    if (ArrLeft) {
+        playerLocation.x -= 5;
+        ctx.clearRect(0, 0, windowX, windowY)
+        ctx.drawImage(playerImg, playerLocation.x, playerLocation.y, 50, 50)
+    } else if (ArrRight) {
+        playerLocation.x += 5;
+        ctx.clearRect(0, 0, windowX, windowY)
+        ctx.drawImage(playerImg, playerLocation.x, playerLocation.y, 50, 50)
+    } else if (ArrUp) {
+        playerLocation.y -= 5;
+        ctx.clearRect(0, 0, windowX, windowY)
+        ctx.drawImage(playerImg, playerLocation.x, playerLocation.y, 50, 50)
+    } else if (ArrDown) {
+        playerLocation.y += 5;
+        ctx.clearRect(0, 0, windowX, windowY)
+        ctx.drawImage(playerImg, playerLocation.x, playerLocation.y, 50, 50)
+    } 
+};
 
 function animate() {
     ctx.clearRect(0, 0, windowX, windowY)
+    ctx.drawImage(playerImg, playerLocation.x, playerLocation.y, 50, 50)
 
     bullets.forEach((element, i) => {
         drawBullet(element.x, element.y)
         update(i)
     });
 
-    ctx.drawImage(playerImg, windowX / 2 - 25, windowY / 2, 50, 50)
-
     requestAnimationFrame(animate);
-}
+};
 
-canvas.addEventListener('click', () => {
-    shoot()
+window.addEventListener('keydown', (e) => {
+    // rotatePlayer()
 
-    if (click) {
-        animate();
-        click = false
+    if (e.key === ' ') {
+        shoot();
+
+        if (isPressed) {
+            animate();
+            isPressed = false;
+        }
+    } else if(e.key === 'ArrowLeft') {
+        update(undefined, true)
+    } else if(e.key === 'ArrowRight') {
+        update(undefined, false, true)
+    } else if(e.key === 'ArrowUp') {
+        update(undefined, false, false, true)
+    } else if(e.key === 'ArrowDown') {
+        update(undefined, false, false, false, true)
     }
-})
+});
 
-draw()
+draw();
